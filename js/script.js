c8d3fe80f3c4efa2f4c54772476cfa98f50de644
GLCVLL <g.ioele96@outlook.it>
2023-05-25 16:52:29 +0200

    fetching item from DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('JS OK');
+
+
+// # OPERAZIONI PRELIMINARI
+
+//RECUPERO IL PLACEHOLDER DAL DOM
+
+const passwordPlaceholder = document.getElementById('personal-password');
+console.log(passwordPlaceholder);
